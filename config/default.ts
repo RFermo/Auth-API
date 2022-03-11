75700679f16e85f1cdabe8bdea4e5a8c18a1d1ad
Randolph <rfermo9807@gmail.com>
2022-03-11 15:02:14 -0600

    Auth API

---
+++ config/default.ts
@@ -0,0 +1,14 @@
+export default {
+    port: 3000,
+    dbUri: "mongodb://localhost:27017/user-api-tutorial",
+    logLevel: "info",
+    accessTokenPrivateKey: "",
+    refreshTokenPrivateKey: "",
+    smtp: {
+        user: 'rfermo9807@gmail.com',
+        pass: 'pmygtmdtwygbkfyz',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true
+    },
+};
